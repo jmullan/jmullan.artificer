ae73c725567f65e290e25cef68387ca10afdf1cd
Jesse Mullan <jmullan@gmail.com>
2025-09-27 10:26:07 -0700

    Move actions to correct location

--- tests/jmullan/artificer/test_sonatype.py
+++ tests/jmullan/artificer/test_sonatype.py
@@ -1,46 +1,49 @@
 """Tests for `jmullan.artificer.artificer`"""
-from jmullan.artificer import sonatype
+
 import pytest
 
+from jmullan.artificer import sonatype
+
+
 def test_force_list():
-    assert [] == sonatype.force_a_list([])
-    assert ["foo"] == sonatype.force_a_list(["foo"])
+    assert sonatype.force_a_list([]) == []
+    assert sonatype.force_a_list(["foo"]) == ["foo"]
 
-    assert ["foo"] == sonatype.force_a_list(("foo",))
+    assert sonatype.force_a_list(("foo",)) == ["foo"]
 
-    assert ["foo"] == sonatype.force_a_list({"foo"})
+    assert sonatype.force_a_list({"foo"}) == ["foo"]
 
-    assert ["foo"] == sonatype.force_a_list("foo")
+    assert sonatype.force_a_list("foo") == ["foo"]
 
-    assert [7] == sonatype.force_a_list(7)
-    assert [7.0] == sonatype.force_a_list(7.0)
+    assert sonatype.force_a_list(7) == [7]
+    assert sonatype.force_a_list(7.0) == [7.0]
 
-    assert ["foo"] == sonatype.force_a_list(b"foo")
+    assert sonatype.force_a_list(b"foo") == ["foo"]
 
-    assert [] == sonatype.force_a_list(None)
+    assert sonatype.force_a_list(None) == []
 
     with pytest.raises(TypeError):
         sonatype.force_a_list({"foo": "bar"})
 
 
 def test_load_json_or_python():
-    assert [] == sonatype.load_json_or_python("[]")
-    assert ["foo"] == sonatype.load_json_or_python('["foo"]')
-    assert ["foo"] == sonatype.load_json_or_python("['foo']")
-    assert 7 == sonatype.load_json_or_python("7")
-    assert None == sonatype.load_json_or_python("null")
-    assert None == sonatype.load_json_or_python("None")
+    assert sonatype.load_json_or_python("[]") == []
+    assert sonatype.load_json_or_python('["foo"]') == ["foo"]
+    assert sonatype.load_json_or_python("['foo']") == ["foo"]
+    assert sonatype.load_json_or_python("7") == 7
+    assert sonatype.load_json_or_python("null") is None
+    assert sonatype.load_json_or_python("None") is None
 
 
 def test_repo_validation():
     repo = sonatype.Repo("foo", "foo", "foo")
     assert repo is not None
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):  # noqa: PT011
         sonatype.Repo(None, "foo", "foo")
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):  # noqa: PT011
         sonatype.Repo("foo", None, "foo")
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):  # noqa: PT011
         sonatype.Repo("foo", "foo", None)
